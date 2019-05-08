2c10b850004918588288bfcadeb709ad49d71ac1
Carlos Cunha <ccfontes@gmail.com>
2019-05-09 00:09:42 +0900

    [codecov badge] lein cloverage looking for failing tests fixed

--- project.clj
+++ project.clj
@@ -10,15 +10,18 @@
   :source-paths ["src/cljc"]
   :test-paths ["test/pass/cljc" "test/fail/cljc"]
   :deploy-repositories [["releases" :clojars]]
-  :aliases {"clj-test-pass" ["test" "eg.test.pass"]
-            "cljs-test-pass" ["with-profiles" "+test-pass" "cljsbuild" "test"]
-            "clj-test-fail" ["test" "eg.test.fail"]
-            "cljs-test-fail" ["with-profiles" "+test-fail" "cljsbuild" "test"]}
-  :plugins [[lein-cljsbuild "1.1.7"] [lein-cloverage "1.1.1"]]
+  :aliases {"clj-test-pass"  ["test" "eg.test.pass"]
+            "cljs-test-pass" ["with-profiles" "+cljs-test-pass" "cljsbuild" "test"]
+            "clj-test-fail"  ["test" "eg.test.fail"]
+            "cljs-test-fail" ["with-profiles" "+cljs-test-fail" "cljsbuild" "test"]
+            "coverage"       ["with-profiles" "+cloverage" "cloverage" "--codecov"]}
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :profiles
     {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
                           [org.clojure/clojurescript "1.10.520" :scope "provided"]]}
-     :test-pass
+     :cloverage {:plugins [[lein-cloverage "1.1.1"]]
+                 :cloverage {:test-ns-regex [#"^eg\.test\.pass$"]}}
+     :cljs-test-pass
       {:cljsbuild
         {:test-commands {"pass-node" ["node" "target/out/test/pass/runner.js"]}
          :builds
@@ -29,7 +32,7 @@
                             :output-to     "target/out/test/pass/runner.js"
                             :output-dir    "target/out/test/pass"
                             :optimizations :none}}}}}
-     :test-fail
+     :cljs-test-fail
       {:cljsbuild
         {:test-commands {"fail-node" ["node" "target/out/test/fail/runner.js"]}
          :builds
