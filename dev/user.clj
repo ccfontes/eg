7e0633f81494a9d7945f636c99dbbd94257ef97f
Carlos Cunha <ccfontes@gmail.com>
2019-05-26 09:25:37 +0900

    Intern 'run-tests' & 'refresh' in proj only ns to fix replacing these fns in ns already using them

--- dev/user.clj
+++ dev/user.clj
@@ -1,13 +1,14 @@
 (ns user
-  (:require [clojure.tools.namespace.repl]
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
             [clojure.test :refer [deftest is]]
             [eg :refer [eg ge ex]]
             [eg.test.pass]))
 
-(defn run-tests- []
-  (clojure.tools.namespace.repl/refresh)
+(defn run-tests []
+  (refresh)
   (clojure.test/run-tests 'eg.test.pass 'user))
 
-(intern 'clojure.core 'refresh clojure.tools.namespace.repl/refresh)
-
-(intern 'clojure.core 'run-tests run-tests-)
+(intern 'eg 'refresh refresh)
+(intern 'eg.platform 'refresh refresh)
+(intern 'eg 'run-tests run-tests)
+(intern 'eg.platform 'run-tests run-tests)
