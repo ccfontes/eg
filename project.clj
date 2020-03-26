e45775c6ef043b68ed9174a0e3ba3fcb2376378e
Carlos Cunha <ccfontes@gmail.com>
2020-03-26 21:05:25 +0900

    Bump to version: 0.4.14-alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eg "0.4.13-alpha"
+(defproject eg "0.4.14-alpha"
   :description "eg delivers clojure.test function tests with conciseness."
   :license {:name "The Universal Permissive License (UPL), Version 1.0"
             :url "https://opensource.org/licenses/UPL"}
