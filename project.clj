c4c7ad48ee640b76ba2af17bb30e9fbaaabea1ff
Carlos da Cunha Fontes <ccfontes@gmail.com>
2020-10-05 20:40:26 +0900

    Bump to version: 0.5.2-alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eg "0.5.1-alpha"
+(defproject eg "0.5.2-alpha"
   :description "eg delivers clojure.test function tests with conciseness."
   :license {:name "The MIT License"
             :url "https://github.com/ccfontes/eg/blob/master/LICENSE.md"}
