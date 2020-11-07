b731e76227df19423a10f8de394c12d7296885b3
Carlos da Cunha Fontes <ccfontes@gmail.com>
2020-11-07 23:37:40 +0900

    Fix run_all_passing_tests.sh script

--- script/run_all_passing_tests.sh
+++ script/run_all_passing_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-lein clj-test-pass-unit
+lein clj-test-pass-unit &&
 lein clj-test-pass-integration | tee /dev/tty | (! grep -qE 'FAIL|ERROR|Error') &&
 lein cljs-test-pass-unit | tee /dev/tty | (! grep -qE 'FAIL|ERROR|Error') &&
 lein cljs-test-pass-integration &&
