b185830894dd5eda247146b9592c90d4db4d1c03
Carlos da Cunha Fontes <ccfontes@gmail.com>
2020-11-14 12:53:07 +0900

    Add assertions == failures check on failing tests

--- script/run_all_passing_tests.sh
+++ script/run_all_passing_tests.sh
@@ -4,4 +4,6 @@ lein clj-test-pass-unit &&
 lein clj-test-pass-integration | tee /dev/tty | (! grep -qE 'FAIL|ERROR|Error') &&
 lein cljs-test-pass-unit | tee /dev/tty | (! grep -qE 'FAIL|ERROR|Error') &&
 lein cljs-test-pass-integration &&
-lein tach planck
+lein tach planck &&
+lein clj-test-fail | tee /dev/tty | rg --quiet --multiline '24 assertions.*\n.*24 failures' &&
+lein cljs-test-fail | tee /dev/tty | rg --quiet --multiline '24 assertions.*\n.*24 failures'
